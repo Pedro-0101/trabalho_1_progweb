86637f8a225f53775d7e8ccd1d39b36b5e297c6f
pedro-0101m <pedro.tiete@outlook.com>
2025-05-30 21:32:14 -0300

    Criado server

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,9 @@ var bodyParser = require('body-parser');
 var app = express();
 
 
+app.use(bodyParser.json());
+var port = 3090;
+
+app.listen(port, function () {
+  console.log('Server is running on port 3000');
+});
