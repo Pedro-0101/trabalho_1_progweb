8b932c86734e5c1f3e11fa213fc09d1ff2ce3a4c
pedro-0101m <pedro.tiete@outlook.com>
2025-06-22 14:24:27 -0300

    corrigido categoria usuario

--- src/model/CategoriaUsuario.ts
+++ src/model/CategoriaUsuario.ts
@@ -1,11 +1,35 @@
+import { CategoriaUsuarioRepository } from "../repository/CategoriaUsuarioRepository";
+
 export class CategoriaUsuario {
     id: number;
     nome: string;
 
-    constructor(id: number, nome: string) {
+    private categoriaUsuarioRepository: CategoriaUsuarioRepository;
 
-        this.id = id;                                               // Atribui o ID da categoria, validacao realizada no service
-        this.nome = nome;                                           // Atribui o nome da categoria, validacao realizada no service
+    constructor(nome: string) {
+
+        this.categoriaUsuarioRepository = CategoriaUsuarioRepository.getInstance();
+
+        // Valida o nome da categoria
+        if (!nome || nome.trim() === "" || nome.length <= 3) {
+            throw new Error("O nome da categoria deve ter mais de 3 caracteres válidos.");
+        }
+
+        // Remove espaços em branco extras e capitaliza a primeira letra
+        nome = nome.trim();
+        nome = nome.charAt(0).toUpperCase() + nome.slice(1).toLowerCase();
+
+        // Verifica se a categoria já existe
+        let categoriaExistente = this.categoriaUsuarioRepository.getListaCategoriasUsuarios().find(categoria => categoria.nome === nome);
+        if (categoriaExistente) {
+            throw new Error("Categoria já existe.");
+        }
+
+        // Cria uma nova categoria com um ID único
+        let id = this.categoriaUsuarioRepository.getListaCategoriasUsuarios().length + 1;
+
+        this.id = id;
+        this.nome = nome;
         
     }
 }
--- src/service/CategoriaUsuarioService.ts
+++ src/service/CategoriaUsuarioService.ts
@@ -9,24 +9,8 @@ export class CategoriaUsuarioService {
     }
 
     public criarCategoriaUsuario(nome: string): CategoriaUsuario {
-        // Valida o nome da categoria
-        if (!nome || nome.trim() === "" || nome.length < 3) {
-            throw new Error("O nome da categoria deve ter mais de 3 caracteres válidos.");
-        }
-
-        // Remove espaços em branco extras e capitaliza a primeira letra
-        nome = nome.trim();
-        nome = nome.charAt(0).toUpperCase() + nome.slice(1).toLowerCase();
-
-        // Verifica se a categoria já existe
-        let categoriaExistente = this.categoriaUsuarioRepository.getListaCategoriasUsuarios().find(categoria => categoria.nome === nome);
-        if (categoriaExistente) {
-            throw new Error("Categoria já existe.");
-        }
-
-        // Cria uma nova categoria com um ID único
-        let id = this.categoriaUsuarioRepository.getListaCategoriasUsuarios().length + 1;
-        let categoriaUsuario = new CategoriaUsuario(id, nome);
+        
+        let categoriaUsuario = new CategoriaUsuario(nome);
 
         // Verifica se a categoria foi criada corretamente
         if (!categoriaUsuario) {
