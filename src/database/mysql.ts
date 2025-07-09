21a77fbe7170b75af9f40363f2a744cc1e8f1daa
pedro-0101m <pedro.tiete@outlook.com>
2025-07-09 19:23:01 -0300

    criada funcao para executar querys sql

--- src/database/mysql.ts
+++ src/database/mysql.ts
@@ -1,3 +1,4 @@
+import { rejects } from 'assert';
 import mysql, { Connection, QueryError } from 'mysql2';
 
 const dbConfig = {
@@ -17,3 +18,16 @@ mysqlConnection.connect((err) => {
     }
     console.log('Conexao bem sucedida com o banco de dados');
 })
+
+export function executeQuery(query: string, valores: any[]): Promise<any>{
+
+    return new Promise ((resolve, reject) => {
+        mysqlConnection.query(query, valores, (err, resultado) => {
+            if(err){
+                console.error('Erro ao executar a query', err);
+                reject(err);
+            }
+            resolve(resultado);
+        });
+    });
+}
